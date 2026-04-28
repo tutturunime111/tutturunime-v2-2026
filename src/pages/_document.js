b836925cd2724fe11cccfc819dca5cf99692061d
eren <bayux265@gmail.com>
2026-04-28 18:47:17 +0700

    tutturuu1@proton.me

---
+++ src/pages/_document.js
@@ -0,0 +1,42 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export default function Document() {
+  return (
+<Html lang="en-ID"  >
+  <Head>
+
+  <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5TJPLS5Z');
+              `,
+            }}
+          />
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-06PQQDD65L"></Script>
+<Script strategy="afterInteractive" id='ga2-google-script'>
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-06PQQDD65L');
+  `}
+</Script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6330491343267680"></script>
+  </Head>
+      <body>
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TJPLS5Z"
+ ></iframe>
+ </noscript>
+  <Main />
+<NextScript />
+        
+      </body>
+    </Html>
+  )
+}
